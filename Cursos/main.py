5a1c116eabe06855abe47692a0a2e55ee4af04ad
nilberthsouza <nilberth@protonmail.com>
2020-01-20 13:37:01 -0300

    visualização de dados adcionada

--- Cursos/main.py
+++ Cursos/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 
-
+fig = plt.figure(figsize=(20,6))
 
 dados_names = ['Data','Valor','Vendedor','LinkImage','ID Produto','Quantidade']
 dproduto = ['ID Produto','Produto','Tipo']
@@ -25,6 +26,45 @@ dados['Vendedor']= dados['Vendedor'].map(lambda a: a.replace('_##',''))
 
 
 df_merged = pd.merge(dados,dproduto,on="ID Produto",how="right")
-print(dados.head())
-print(dproduto.head())
+
 print(df_merged.head())
+
+#numero de vendas, contando ocorrencia de ID Produtos
+print(len(dados['ID Produto']))
+
+#numero de vendas por vendendor e contruir grafico
+plt.subplot2grid((3,3),(0,0))
+dados['Vendedor'].value_counts(normalize=True).plot(kind="bar")
+plt.title('Vendas por Vendedor')
+
+#calculando receita
+dados['Valor venda'] = dados['Valor']*dados['Quantidade']
+
+Receita = "R$"+str(dados['Valor venda'].sum())
+plt.subplot2grid((3,3),(0,1))
+plt.text(0.0,0.1,Receita,size =43)
+plt.title("Receita Total")
+
+plt.subplot2grid((3,3),(0,2))
+df_merged['Produto'].value_counts(normalize=True).plot(kind="bar")
+plt.title("Numero de vendas por produto")
+
+plt.subplot2grid((3,3),(0,2))
+df_merged.Vendedor[df_merged['ID Produto'] == 1].value_counts(normalize=True).plot(kind='bar')
+plt.title("Produto 1 por Vendedor")
+
+
+plt.subplot2grid((3,3),(1,1))
+df_merged.Vendedor[df_merged['ID Produto'] == 2].value_counts(normalize=True).plot(kind='bar')
+plt.title("Produto 2 por Vendedor")
+
+
+plt.subplot2grid((3,3),(1,0))
+df_merged.Vendedor[df_merged['ID Produto'] == 3].value_counts(normalize=True).plot(kind='bar')
+plt.title("Produto 3 por Vendedor")
+
+plt.subplot2grid((3,3),(1,2))
+df_merged.Produto[df_merged['Vendedor'] == 'Trovato'].value_counts(normalize=True).plot(kind='bar')
+plt.title("Produtos Vendidos por Trovato")
+
+plt.show()
